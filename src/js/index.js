c8d71a2f249e9ec25f1f80c1aef75f17353fc135
XNORF <78629634+XNORF@users.noreply.github.com>
2024-03-06 18:41:56 +0800

    simple login

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,19 @@
 const main = require("./main.js");
 /////////////////////////////////////////////////////////////////
 
-console.log("Hello world!");
+const loginForm = document.querySelector("#loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+    console.log(email + " " + password);
+    main.signInWithEmailAndPassword(main.auth, email, password)
+        .then((cred) => {
+            alert("Sign in successful");
+            window.location.href = "/home.html";
+        })
+        .catch((error) => {
+            console.log(error.code);
+        });
+});
