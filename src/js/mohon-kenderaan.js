ed645dcf0df9ed38a72ba291fc04d139d7b97d8a
Daliligit config --global user.name Dalili <dalilifaiqah@gmail.com>
2024-09-01 01:40:15 +0800

    Kenderaan Form done

--- src/js/mohon-kenderaan.js
+++ src/js/mohon-kenderaan.js
@@ -1,3 +1,16 @@
 ////////////////////////////MUST HAVE////////////////////////////
 const main = require("./main.js");
 /////////////////////////////////////////////////////////////////
+function toggleEndDate() {
+    var daysSelect = document.getElementById("days");
+    var endDateInput = document.getElementById("end_date");
+
+    if (daysSelect.value === "balik_hari") {
+        endDateInput.disabled = true;
+        endDateInput.required = false;
+        endDateInput.value = ""; // Clear the value
+    } else {
+        endDateInput.disabled = false;
+        endDateInput.required = true;
+    }
+}
