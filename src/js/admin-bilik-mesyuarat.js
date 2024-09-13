d74a815dab81cb511ae06ade00d550cc2e2f7349
Daliligit config --global user.name Dalili <dalilifaiqah@gmail.com>
2024-09-14 04:53:48 +0800

    Admin bilik mesyuarat done

--- src/js/admin-bilik-mesyuarat.js
+++ src/js/admin-bilik-mesyuarat.js
@@ -2,3 +2,46 @@
 import $ from "jquery";
 const main = require("./main.js");
 /////////////////////////////////////////////////////////////////
+new DataTable('#example');
+
+//untuk modal
+// For Tambah Pengguna modal
+var modaladd = document.getElementById("ModalTambahBilik");
+var btnadd = document.getElementById("TambahBilik");
+var spanadd = document.getElementsByClassName("close-tambah")[0]; // Target the unique class
+
+btnadd.onclick = function() {
+    modaladd.style.display = "block";
+}
+
+spanadd.onclick = function() {
+    modaladd.style.display = "none";
+}
+
+// For Kemas Kini Pengguna modal
+var modalupdate = document.getElementById("ModalKemasKiniBilik");
+var btnupdate = document.getElementById("KemasKiniBilik");
+var spanupdate = document.getElementsByClassName("close-update")[0]; // Target the unique class
+
+btnupdate.onclick = function() {
+    modalupdate.style.display = "block";
+}
+
+spanupdate.onclick = function() {
+    modalupdate.style.display = "none";
+}
+
+// Close the modals when clicking outside the modal box
+window.onclick = function(event) {
+    // Close Tambah Pengguna modal if clicked outside
+    if (event.target == modaladd) {
+        modaladd.style.display = "none";
+    }
+    
+    // Close Kemas Kini Pengguna modal if clicked outside
+    if (event.target == modalupdate) {
+        modalupdate.style.display = "none";
+    }
+}
+
+//end modal
